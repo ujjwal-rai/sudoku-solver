ab44dd39b8981a957699c8b51a8698750cc788c2
Ujjwal Rai <96328364+ujjwal-rai@users.noreply.github.com>
2023-12-28 00:38:17 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,29 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 #define N 9
 
-void print(int arr[N][N]) {
-    for (int i = 0; i < N; i++) {
+void print(int arr[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
         for (int j = 0; j < N; j++)
+        {
             printf("%d ", arr[i][j]);
+        }
         printf("\n");
     }
 }
 
-int isSafe(int grid[N][N], int row, int col, int num) {
+int isSafe(int grid[N][N], int row, int col, int num)
+{
     for (int x = 0; x < N; x++)
+    {
         if (grid[row][x] == num)
             return 0;
+    }
 
     for (int x = 0; x < N; x++)
+    {
         if (grid[x][col] == num)
             return 0;
+    }
 
     int startRow = row - row % 3;
     int startCol = col - col % 3;
 
     for (int i = 0; i < 3; i++)
+    {
         for (int j = 0; j < 3; j++)
+        {
             if (grid[i + startRow][j + startCol] == num)
                 return 0;
+        }
+    }
 
     return 1;
+}
+int main()
+{
+    return 0;
 }
