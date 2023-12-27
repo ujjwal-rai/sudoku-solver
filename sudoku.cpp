28cadc7b03519de5a643f0332e26eaefa7c9a3c8
Ujjwal Rai <96328364+ujjwal-rai@users.noreply.github.com>
2023-12-28 00:56:05 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,49 +1,109 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 #define N 9
 
-void print(int arr[N][N])
+void printGrid(int grid[N][N])
 {
-    for (int i = 0; i < N; i++)
+    for(int row = 0; row < N; row++)
     {
-        for (int j = 0; j < N; j++)
+        for(int col = 0; col < N; col++)
         {
-            printf("%d ", arr[i][j]);
+            cout << grid[row][col] << " ";
         }
-        printf("\n");
+        cout << endl;
     }
 }
 
-int isSafe(int grid[N][N], int row, int col, int num)
+bool isSafe(int grid[N][N], int row, int col, int num)
 {
-    for (int x = 0; x < N; x++)
+    // Check if 'num' is not present in the current row and column
+    for(int x = 0; x < N; x++)
     {
-        if (grid[row][x] == num)
-            return 0;
+        if(grid[row][x] == num || grid[x][col] == num)
+        {
+            return false;
+        }
     }
 
-    for (int x = 0; x < N; x++)
-    {
-        if (grid[x][col] == num)
-            return 0;
-    }
-
+    // Check if 'num' is not present in the current 3x3 subgrid
     int startRow = row - row % 3;
     int startCol = col - col % 3;
-
-    for (int i = 0; i < 3; i++)
+    for(int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for(int j = 0; j < 3; j++)
         {
-            if (grid[i + startRow][j + startCol] == num)
-                return 0;
+            if(grid[i + startRow][j + startCol] == num)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(int grid[N][N])
+{
+    int row, col;
+
+    // Find an unassigned location
+    bool found = false;
+    for(row = 0; row < N; row++)
+    {
+        for(col = 0; col < N; col++)
+        {
+            if(grid[row][col] == 0)
+            {
+                found = true;
+                break;
+            }
+        }
+        if(found)
+        {
+            break;
         }
     }
 
-    return 1;
+    // If no unassigned location is found, the Sudoku is solved
+    if(!found)
+    {
+        return true;
+    }
+
+    // Try filling the current location with numbers 1 to 9
+    for(int num = 1; num <= 9; num++)
+    {
+        if(isSafe(grid, row, col, num))
+        {
+            grid[row][col] = num;
+            // Recursively solve the rest of the Sudoku
+            if(solveSudoku(grid))
+            {
+                return true;
+            }
+            // If placing 'num' at the current location doesn't lead to a solution,
+            // backtrack and try the next number
+            grid[row][col] = 0;
+        }
+    }
+
+    // No number can be placed at the current location, backtrack
+    return false;
 }
+
 int main()
 {
+    int grid[N][N] = {{5, 3, 0, 0, 7, 0, 0, 0, 0}, {6, 0, 0, 1, 9, 5, 0, 0, 0}, {0, 9, 8, 0, 0, 0, 0, 6, 0}, {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1}, {7, 0, 0, 0, 2, 0, 0, 0, 6}, {0, 6, 0, 0, 0, 0, 2, 8, 0}, {0, 0, 0, 4, 1, 9, 0, 0, 5}, {0, 0, 0, 0, 8, 0, 0, 7, 9}};
+
+    if(solveSudoku(grid))
+    {
+        printGrid(grid);
+    }
+    else
+    {
+        cout << "No solution exists." << endl;
+    }
+
     return 0;
 }
